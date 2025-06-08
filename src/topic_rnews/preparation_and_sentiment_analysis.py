07c7cf3be0e0aa698d5c40ec4669ec01d3d0aa0c
Kara Kuebart <nkuebart@uni-bonn.de>
2025-06-08 12:49:55 +0200

    restart from sent_a

--- src/topic_rnews/preparation_and_sentiment_analysis.py
+++ src/topic_rnews/preparation_and_sentiment_analysis.py
@@ -15,29 +15,33 @@ if __name__ == "__main__":
     # initialize parallel pandas
     args = read_data.get_args()
     ParallelPandas.initialize(n_cpu=args.no_of_cpu_cores, split_factor=4)
-    # import data from numpy arrays
-    print(datetime.datetime.now(), ': beginning import')
 
-    news_df = read_data.create_dataframe(args)
-    print(datetime.datetime.now(), f': dataframe created (size:{len(news_df.index)}), joining headings and paragraphs:')
+    # # import data from numpy arrays
+    # print(datetime.datetime.now(), ': beginning import')
+    #
+    # news_df = read_data.create_dataframe(args)
+    # print(datetime.datetime.now(), f': dataframe created (size:{len(news_df.index)}), joining headings and paragraphs:')
+    #
+    # # add headings and corresponding paragraphs together (if confidence is high).
+    # # news_df = preprocessing.join_headings_w_paragraphs(news_df) # DEACTIVATED BECAUSE IT TAKES TOO LONG
+    #
+    # # drop unuseful data
+    # news_df = preprocessing.drop_short_lines(news_df)
+    #
+    # # preprocessing: lowercase,
+    #     # consolidations (long ſ -> s, ä ö ü ß -> ae oe ue ss),
+    #     # lemmata (OCR corrections),
+    #     # stopword removal,
+    #     # punctuation removal:
+    # news_df = preprocessing.main(news_df)
+    # news_df.reset_index(drop=True, inplace=True)
+    #
+    #
+    # # save preliminary results
+    # news_df.to_csv(args.output_document_path + '_pre.csv', sep=';', index=False)
 
-    # add headings and corresponding paragraphs together (if confidence is high).
-    news_df = preprocessing.join_headings_w_paragraphs(news_df)
-
-    # drop unuseful data
-    news_df = preprocessing.drop_short_lines(news_df)
-
-    # preprocessing: lowercase,
-        # consolidations (long ſ -> s, ä ö ü ß -> ae oe ue ss),
-        # lemmata (OCR corrections),
-        # stopword removal,
-        # punctuation removal:
-    news_df = preprocessing.main(news_df)
-    news_df.reset_index(drop=True, inplace=True)
-
-
-    # save preliminary results
-    news_df.to_csv(args.output_document_path + '_pre.csv', sep=';', index=False)
+    # if need be, load df:
+    news_df = pd.read_csv(args.output_document_path + '_pre.csv', sep=';')
 
     # ANALYSIS
 
