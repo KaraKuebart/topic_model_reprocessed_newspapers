db61ee67222472bde7c4ca9afc0135d4083f6746
Kara Kuebart <nkuebart@uni-bonn.de>
2025-09-20 19:39:05 +0200

    speed up reintegrate_text.py

--- src/topic_rnews/reintegrate_text.py
+++ src/topic_rnews/reintegrate_text.py
@@ -11,11 +11,10 @@ import glob
 from parallel_pandas import ParallelPandas
 import datetime
 
-ParallelPandas.initialize(n_cpu=16, split_factor=4, disable_pr_bar=False)
+ParallelPandas.initialize(n_cpu=96, split_factor=4, disable_pr_bar=False)
 
 def insert_row_text(row):
-    index = raw_df[(raw_df['path'] == row['path']) & (raw_df['region'] == row['region'])].index[0]
-    return raw_df.at[index, 'text']
+    return raw_df.at[row['ID'], 'text']
 
 
 def clean_df(df):
@@ -23,15 +22,18 @@ def clean_df(df):
         df.drop(columns=['text'], inplace=True)
     if 'Unnamed: 0' in df.columns:
         df.drop(columns=['Unnamed: 0'], inplace=True)
-    if 'Unnamed: 1' in df.columns:
+    if 'Unnamed: 0.1' in df.columns:
         df.drop(columns=['Unnamed: 0.1'], inplace=True)
-    if 'Unnamed: 2' in df.columns:
-        df.drop(columns=['Unnamed: 0.1'], inplace=True)
+    if 'Unnamed: 0.01' in df.columns:
+        df.drop(columns=['Unnamed: 0.01'], inplace=True)
+    df['ID'] = df['path'].astype(str) + '_' + df['region'].astype(str)
     return df
 
 if __name__ == "__main__":
     # read all_raw.csv
     raw_df = pd.read_csv('input/all_raw.csv', sep=';')
+    raw_df['ID'] = raw_df['path'].astype(str) + '_' + raw_df['region'].astype(str)
+    raw_df.set_index('ID', inplace=True)
     csvfiles = glob.glob('output/*.csv')
     for csvfile in tqdm(csvfiles):
         print(datetime.datetime.now(), f': reintegrating text into: {csvfile}')
@@ -39,5 +41,3 @@ if __name__ == "__main__":
         df = clean_df(df)
         df['text'] = df.p_apply(insert_row_text, axis=1)
         df.to_csv(csvfile, sep=';', index=False)
-
-
